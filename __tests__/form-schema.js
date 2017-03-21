e4673942c112795b71e9a5b7e051248f80a58264
spruce-bruce <aaron@synapsestudios.com>
2017-03-21 16:26:22 -0700

    reorganize tests

--- __tests__/form-schema.js
+++ __tests__/form-schema.js
@@ -1,20 +1,36 @@
 const FormSchema = require('../lib/form-schema');
 
-test('form initializes without error', () => {
-  const formSchema = new FormSchema(); // eslint-disable-line no-unused-vars
-  expect(true);
-});
 
-test('calling getFormSchema on a new form returns a blank form schema', () => {
-  const formSchema = new FormSchema();
-
-  expect(formSchema.getFormSchema()).toEqual({
-    name: '',
-    fields: [],
+describe('initialization', () => {
+  test('form initializes without error', () => {
+    const formSchema = new FormSchema(); // eslint-disable-line no-unused-vars
+    expect(true);
   });
 });
 
-test('calling serialize returns a blank schema as a json string', () => {
-  const formSchema = new FormSchema();
-  expect(formSchema.serialize()).toBe("{\"name\":\"\",\"fields\":[]}");
+describe('basic usage', () => {
+  let formSchema;
+  beforeEach(() => {
+    formSchema = new FormSchema();
+  });
+
+  test('calling getFormSchema on a new form returns a blank form schema', () => {
+
+    expect(formSchema.getFormSchema()).toEqual({
+      name: '',
+      fields: [],
+    });
+  });
+
+  test('calling serialize returns a blank schema as a json string', () => {
+    expect(formSchema.serialize()).toBe("{\"name\":\"\",\"fields\":[]}");
+  });
+
+  test('calling addField adds a field with the correct name and type', () => {
+    formSchema.addField('name', 'text');
+
+    const schema = formSchema.getFormSchema();
+    expect(schema.fields[0].name).toBe('name');
+    expect(schema.fields[0].element).toBe('text');
+  });
 });
