1d9246e1953b705e2ffcfc9c77e685e2e5d8737f
spruce-bruce <aaron@synapsestudios.com>
2017-03-22 12:39:22 -0700

    remove fields

--- __tests__/form-schema.js
+++ __tests__/form-schema.js
@@ -9,75 +9,119 @@ describe('initialization', () => {
 });
 
 describe('basic usage', () => {
-  let formSchema;
-  beforeEach(() => {
-    formSchema = new FormSchema();
-  });
+  describe('adding fields', () => {
+    let formSchema;
+    beforeEach(() => {
+      formSchema = new FormSchema();
+    });
 
-  test('calling getFormSchema on a new form returns a blank form schema', () => {
+    test('calling getFormSchema on a new form returns a blank form schema', () => {
 
-    expect(formSchema.getFormSchema()).toEqual({
-      name: '',
-      fields: [],
+      expect(formSchema.getFormSchema()).toEqual({
+        name: '',
+        fields: [],
+      });
+    });
+
+    test('calling serialize returns a blank schema as a json string', () => {
+      expect(formSchema.serialize()).toBe("{\"name\":\"\",\"fields\":[]}");
+    });
+
+    test('calling addField adds a field with the correct name and type', () => {
+      formSchema.addField('name', 'hidden');
+
+      const schema = formSchema.getFormSchema();
+      expect(schema.fields[0].name).toBe('name');
+      expect(schema.fields[0].type).toBe('hidden');
+    });
+
+    test('calling AddField with an object', () => {
+      formSchema.addField({
+        name: 'form name',
+        type: 'text',
+        label: 'Form Name',
+        value: 'Some Value',
+      });
+
+      const schema = formSchema.getFormSchema();
+      expect(schema.fields[0].name).toBe('form name');
+      expect(schema.fields[0].type).toBe('text');
+      expect(schema.fields[0].label).toBe('Form Name');
+      expect(schema.fields[0].value).toBe('Some Value');
+    });
+
+    test('calling addField with missing name in object throws an error', () => {
+      function callAddFieldErroneously() {
+        formSchema.addField({ type: 'hi' });
+      }
+
+      expect(callAddFieldErroneously).toThrow(/name is required/);
+    });
+
+    test('calling addField with null name throws an error', () => {
+      function callAddFieldErroneously() {
+        formSchema.addField(null);
+      }
+
+      expect(callAddFieldErroneously).toThrow(/name is required/);
+    });
+
+    test('calling addField with missing type in object throws an error', () => {
+      function callAddFieldErroneously() {
+        formSchema.addField({ name: 'hi' });
+      }
+
+      expect(callAddFieldErroneously).toThrow(/type is required/);
+    });
+
+    test('calling addField with undefined type throws an error', () => {
+      function callAddFieldErroneously() {
+        formSchema.addField('hi');
+      }
+
+      expect(callAddFieldErroneously).toThrow(/type is required/);
     });
   });
 
-  test('calling serialize returns a blank schema as a json string', () => {
-    expect(formSchema.serialize()).toBe("{\"name\":\"\",\"fields\":[]}");
-  });
+  describe('removing fields', () => {
+    let formSchema;
+    beforeEach(() => {
+      formSchema = new FormSchema();
+      formSchema.addField({
+        name: 'field1',
+        type: 'text',
+      });
 
-  test('calling addField adds a field with the correct name and type', () => {
-    formSchema.addField('name', 'hidden');
+      formSchema.addField({
+        name: 'field2',
+        type: 'text',
+      });
 
-    const schema = formSchema.getFormSchema();
-    expect(schema.fields[0].name).toBe('name');
-    expect(schema.fields[0].type).toBe('hidden');
-  });
-
-  test('calling AddField with an object', () => {
-    formSchema.addField({
-      name: 'form name',
-      type: 'text',
-      label: 'Form Name',
-      value: 'Some Value',
+      formSchema.addField({
+        name: 'field3',
+        type: 'text',
+      })
     });
 
-    const schema = formSchema.getFormSchema();
-    expect(schema.fields[0].name).toBe('form name');
-    expect(schema.fields[0].type).toBe('text');
-    expect(schema.fields[0].label).toBe('Form Name');
-    expect(schema.fields[0].value).toBe('Some Value');
+    test('calling removeField removes the field', () => {
+      expect(formSchema.removeField('field3')).toBe(true);
+      const schema = formSchema.getFormSchema();
+      expect(schema.fields[2]).toBeUndefined();
+    });
+
+    test('calling removeField correctly splices and maintains field order', () => {
+      expect(formSchema.removeField('field2')).toBe(true);
+      const schema = formSchema.getFormSchema();
+      expect(schema.fields[1]).toEqual({
+        name: 'field3',
+        type: 'text',
+      });
+    });
+
+    test('calling removeField on something that does not exist returns false', () => {
+      expect(formSchema.removeField('hello')).toBe(false);
+    });
   });
 
-  test('calling addField with missing name in object throws an error', () => {
-    function callAddFieldErroneously() {
-      formSchema.addField({ type: 'hi' });
-    }
 
-    expect(callAddFieldErroneously).toThrow(/name is required/);
-  });
-
-  test('calling addField with null name throws an error', () => {
-    function callAddFieldErroneously() {
-      formSchema.addField(null);
-    }
-
-    expect(callAddFieldErroneously).toThrow(/name is required/);
-  });
-
-  test('calling addField with missing type in object throws an error', () => {
-    function callAddFieldErroneously() {
-      formSchema.addField({ name: 'hi' });
-    }
-
-    expect(callAddFieldErroneously).toThrow(/type is required/);
-  });
-
-  test('calling addField with undefined type throws an error', () => {
-    function callAddFieldErroneously() {
-      formSchema.addField('hi');
-    }
-
-    expect(callAddFieldErroneously).toThrow(/type is required/);
-  });
 });
